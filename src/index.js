484178931cd5cf8475f47336de8ac19da9a7b41e
PaulNguyen <paulnguyen2207@gmail.com>
2021-11-09 00:48:51 +1100

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+const form = document.querySelector('#create-task-form')
+const input=document.querySelector('#new-task-description')
+const uList=document.querySelector('#tasks')
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  const list=document.createElement("li")
+  list.innerHTML=`${input.value} <button onclick="deleteTask(event)">x</button>`
+  uList.append(list)
+
+})
+
+function deleteTask(e){
+alert ("clicked")
+e.target.parentNode.remove()}
